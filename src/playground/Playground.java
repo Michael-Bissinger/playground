6137141f4c42b73c688112890716cbe98690ad18
Michael Bissinger <Michael.Bissinger@inb.de>
2019-02-15 14:21:51 +0100

    Creating a Rectangle, Circle, Line

--- src/playground/Playground.java
+++ src/playground/Playground.java
@@ -5,11 +5,15 @@
  */
 package playground;
 
+
 import javafx.application.Application;
-import javafx.fxml.FXMLLoader;
 import javafx.scene.Group;
-import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.paint.Color;
+import javafx.scene.shape.Circle;
+import javafx.scene.shape.Line;
+import javafx.scene.shape.Rectangle;
+import static javafx.scene.text.FontWeight.BLACK;
 import javafx.stage.Stage;
 
 /**
@@ -23,8 +27,19 @@ public class Playground extends Application {
         
         Group root = new Group();
         
+        Line seesaw = new Line(60,340,340,140); 
+        seesaw.setStroke(Color.BLACK);
+        seesaw.setStrokeWidth(15);
         
-       
+        Circle cir = new Circle(70,280,40);
+        cir.setStroke(Color.RED);
+        cir.setFill(Color.ORANGE);
+        
+        Rectangle rec = new Rectangle(240,90,80,70);
+        rec.setStroke(Color.GREEN);
+        rec.setStrokeWidth(5);
+        rec.setFill(Color.YELLOWGREEN);
+        
         
         Scene scene = new Scene(root, 400, 400);
         
