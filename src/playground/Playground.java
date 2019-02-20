8ce1340456480a55dea3efa1ef96036c296ac246
Michael Bissinger <Michael.Bissinger@inb.de>
2019-02-20 12:44:32 +0100

    Reset project

--- src/playground/Playground.java
+++ src/playground/Playground.java
@@ -6,20 +6,9 @@
 package playground;
 
 
-import static java.awt.SystemColor.text;
 import javafx.application.Application;
-import javafx.scene.Group;
-import javafx.scene.Node;
 import javafx.scene.Scene;
-import javafx.scene.effect.BlendMode;
-import javafx.scene.layout.BorderPane;
-import javafx.scene.layout.HBox;
 import javafx.scene.layout.StackPane;
-import javafx.scene.layout.VBox;
-import javafx.scene.paint.Color;
-import javafx.scene.shape.Circle;
-import javafx.scene.shape.Rectangle;
-import javafx.scene.text.Text;
 import javafx.stage.Stage;
 
 /**
@@ -32,24 +21,10 @@ public class Playground extends Application {
     public void start(Stage stage) throws Exception {
         
         StackPane root = new StackPane();
-        root.getChildren().add(blendModeObjects());
-        
-        BorderPane pane = new BorderPane();
-        
-        
         Scene scene = new Scene(root, 300, 250);
-        Scene scene2 = new Scene(pane, 400, 450);
+
         
-        
-        
-        pane.setTop(addHBox("Top"));
-        pane.setLeft(addVBox("Left"));
-        pane.setRight(addVBox("Right"));
-        pane.setBottom(addHBox("Bottom"));
-        
-        scene2.getStylesheets().add("playground/textStyles.css");
-        
-        
+   
         // What to show
         stage.setTitle("Adding text");
         //stage.setScene(scene); // Shows the scene with the blended objects
@@ -57,46 +32,10 @@ public class Playground extends Application {
         stage.show();
     }
     
-    public static HBox addHBox(String str) {
-        HBox hbox = new HBox();
-        Text text = new Text(str);
-        
-        hbox.setId("hbox");
-        text.setId("textstyle");
-        hbox.getChildren().add(text);
-        return hbox;
-        
-    }
-    
-    public static VBox addVBox(String str) {
-        VBox vbox = new VBox();
-        Text text = new Text();
-        
-        vbox.setId("vbox");
-        text.setId("textstyle");
-        vbox.getChildren().add(text);
-        return vbox;
-        
-        
-    }
-    
-    static Node blendModeObjects() {
-        Group g = new Group();
-        
-        Circle c = new Circle(50,50,25);
-        c.setFill(Color.DARKCYAN);
-        c.setBlendMode(BlendMode.OVERLAY);
-        
-        Rectangle r = new Rectangle(50,50,50,50);
-        r.setFill(Color.BLUEVIOLET);
-        
-        
-        g.setBlendMode(BlendMode.SRC_ATOP);
-        g.getChildren().addAll(r,c);
-        return g;
-                
-    }
-    
+    /**
+     *
+     */
+
     
     /**
      * @param args the command line arguments
